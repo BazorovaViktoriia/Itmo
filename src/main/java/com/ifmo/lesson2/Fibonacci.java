199e88f512f0499ba2870e3ad9ee494b5b8614c3
V.Guseva <vikitutnet@mail.ru>
2021-10-12 21:00:12 +0300

    some HomeWork (not all)

---
+++ src/main/java/com/ifmo/lesson2/Fibonacci.java
@@ -0,0 +1,19 @@
+package com.ifmo.lesson2;
+
+public class Fibonacci {
+    /*
+     Выведите на экран первые 11 членов последовательности Фибоначчи. Напоминаем, что
+     первый и второй члены последовательности равны единицам, а каждый следующий — сумме
+     двух предыдущих.
+     */
+    public static void main(String[] args) {
+        int [] array = new int[12];
+        array[0]=1;
+        array[1]=1;
+        for (int i=2; i<=11; i++) {
+            array[i]=array[i-2]+array[i-1];
+        }
+
+        for (int i :array) System.out.println(i);
+    }
+}
