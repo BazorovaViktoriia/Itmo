2fec6b8ea193ecefd1a7d3ec9ff4e1615b00a051
V.Guseva <vikitutnet@mail.ru>
2021-10-24 22:25:27 +0300

    Progression

---
+++ src/main/java/com/ifmo/lesson2/Progression.java
@@ -0,0 +1,15 @@
+package com.ifmo.lesson2;
+
+public class Progression {
+    /*
+     Создайте программу, выводящую на экран все четырёхзначные числа последовательности
+     1000 1003 1006 1009 1012 1015 ….
+     */
+    public static void main(String[] args) {
+        int a = 1000;
+       while (true) {
+           System.out.println(a+=3);
+           if (a>=9997) break;
+        }
+    }
+}
