e891a1efeb4ff98912494003547686c0032160d9
V.Guseva <vikitutnet@mail.ru>
2021-10-25 21:52:52 +0300

    NonNegativeSequence

---
+++ src/main/java/com/ifmo/lesson2/NonNegativeSequence.java
@@ -0,0 +1,15 @@
+package com.ifmo.lesson2;
+
+public class NonNegativeSequence {
+    /*
+    Создайте программу, выводящую на экран все неотрицательные элементы
+    последовательности 90 85 80 75 70 65 60 ….
+     */
+    public static void main(String[] args) {
+        int first = 90;
+       while (first>0){
+           System.out.printf(first+" ");
+           first-=5;
+       }
+    }
+}
