62b251bc81c1b16365728d203977775b9e3c2279
V.Guseva <vikitutnet@mail.ru>
2021-10-27 08:45:15 +0300

    PrimeNumber_

--- src/main/java/com/ifmo/lesson2/PrimeNumber.java
+++ src/main/java/com/ifmo/lesson2/PrimeNumber.java
@@ -25,7 +25,7 @@ public class PrimeNumber {
 
     public static boolean isPrime(int n) {
         if (n==1) {
-            return true;
+            return false;
         }
         for(int i=(int)Math.sqrt(n);i>=1;i--) {
             if (n % i == 0 & i != 1) {
