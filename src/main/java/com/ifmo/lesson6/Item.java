d734d49f9b99ef4840c5b4d045efb1a5c8b90799
V.Guseva <vikitutnet@mail.ru>
2021-10-31 22:43:35 +0300

    LinkedList_2

--- src/main/java/com/ifmo/lesson6/Item.java
+++ src/main/java/com/ifmo/lesson6/Item.java
@@ -8,12 +8,12 @@ package com.ifmo.lesson6;
  *     только для LinkedList'a.
  * </p>
  */
-class Item {
+class Item<T> {
     /** Значение элемента. */
-    Object value;
+    T value;
 
     /** Ссылка на следующий элемент. */
-    Item next;
+    Item<T> next;
 
     /**
      * Инициализирует элемент со значением
@@ -22,7 +22,7 @@ class Item {
      * @param value Значение, которое будет сохранено
      *              в этом элементе.
      */
-    Item(Object value) {
+    Item(T value) {
 
         this.value = value;
     }
--- src/main/java/com/ifmo/lesson6/LinkedList.java
+++ src/main/java/com/ifmo/lesson6/LinkedList.java
@@ -7,35 +7,32 @@ import java.util.Iterator;
  * элемент харнит ссылку на следующий. Список
  * оканчивается ссылкой со значением {@code null}.
  */
-public class LinkedList implements List, Stack, Queue {
+public class LinkedList<T> implements List<T>, Stack<T>, Queue<T> {
     /** Ссылка на первый элемент списка. */
-    private Item head;
+    private Item<T> head;
 
     /** {@inheritDoc} */
     @Override
-    public void add(Object val) {
-        // TODO implement.
+    public void add(T val) {
+        Item<T> item = new Item<>(val);
+        if (head==null){
+            head=item;
+        }
+        else {
+            Item current = head;
+            while (true){
+                if (current.next == null){
+                    current.next = item;
+                    break;
+                }
+                current = current.next;
+            }
+        }
     }
 
-    /** {@inheritDoc} */
+
     @Override
-    public Object take() {
-        // TODO implement.
-
-        return null;
-    }
-
-    /** {@inheritDoc} */
-    @Override
-    public Object get(int i) {
-        // TODO implement.
-
-        return null;
-    }
-
-    /** {@inheritDoc} */
-    @Override
-    public Object remove(int i) {
+    public T take() {
         // TODO implement.
 
         return null;
@@ -43,21 +40,54 @@ public class LinkedList implements List, Stack, Queue {
 
     /** {@inheritDoc} */
     @Override
-    public Iterator iterator() {
-        // TODO implement.
+    public T get(int i) {
+        if (i<0||head ==null) {
+            return null;
+        }
+
+        int cnt = 0;
+        Item<T> item = head;
+        while (true){
+            if (cnt==i){
+                return item.value;
+            }
+            item = item.next;
+            cnt++;
+        }
+    }
+
+    @Override
+    public T remove(int i) {
+
 
         return null;
     }
 
+    @Override
+    public Iterator<T> iterator() {
+
+        return new Iterator<T>() {
+            @Override
+            public boolean hasNext() {
+                return false;
+            }
+
+            @Override
+            public T next() {
+                return null;
+            }
+        };
+    }
+
     /** {@inheritDoc} */
     @Override
-    public void push(Object value) {
+    public void push(T value) {
         // TODO implement.
     }
 
     /** {@inheritDoc} */
     @Override
-    public Object pop() {
+    public T pop() {
         // TODO implement.
 
         return null;
