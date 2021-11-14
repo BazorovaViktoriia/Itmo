5aff65327db285ec5096db21eca1e882540a63c1
vikit <vikitutnet@mail.ru>
2021-11-14 21:52:30 +0300

    lesson 14

---
+++ src/main/java/com/ifmo/lesson14/calc/SimpleCalc.java
@@ -0,0 +1,116 @@
+package com.ifmo.lesson14.calc;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+/*
+ * Добавьте поддержку переменных.
+ * Синтаксис следующий.
+ * Объявление переменной через символ '=':
+ * x = 8
+ *
+ * Имя переменной не может быть числом.
+ *
+ * Использование в выражении вместо одного или двух аргументов:
+ * x + 2
+ * Результат: 10.
+ *
+ * Калькулятор должен выбрасывать соответствующие исключения с
+ * подробными описаниями ошибок и как их исправить. Например,
+ * если имя переменной не найдено или использовался неверный синтаксис.
+ */
+public class SimpleCalc {
+
+    private static Map<String, Integer> map = new HashMap<>();
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Enter expression: ");
+
+            String line = scanner.nextLine();
+
+            if ("exit".equals(line))
+                break;
+
+            try {
+
+                System.out.println("Answer is: " + calculate(line));
+            }
+            catch (CalcException e) {
+                System.err.println("Error occurred: ");
+
+                e.printStackTrace();
+            }
+        }
+    }
+    //
+     //if (line.contains("="))
+    //   throw new CalcException("Variable can't contain the finish result");
+
+
+    static int calculate(String line) throws CalcException {
+
+        if (!line.contains("+") && !line.contains("-")&& !line.contains("="))
+            throw new CalcException("Expression must contain '+' or '-': " + line);
+
+        String[] operands = line.split(" ");
+
+        if (operands.length != 3)
+            throw new CalcException("Expression must have only 3 operands separated with space (e.g. 2 + 4): " + line);
+
+        OPERATOR operator = OPERATOR.parse(operands[1]);
+
+        if (OPERATOR.EQUALS.equals(operator)) {
+            int op2 = parseOperand(operands[2]);
+            map.put(operands[0], op2);
+            return op2;
+        }
+
+        int op1 = map.containsKey(operands[0]) ? map.get(operands[0]) : parseOperand(operands[0]);
+        int op2 = map.containsKey(operands[2]) ? map.get(operands[2]) : parseOperand(operands[2]);
+
+        return operator.apply(op1, op2);
+    }
+
+    private static int parseOperand(String string) throws CalcException {
+        try {
+            return Integer.parseInt(string);
+        }
+        catch (NumberFormatException e) {
+            throw new CalcException("Wrong operand, must be only integer number: " + string, e);
+        }
+    }
+
+    private enum OPERATOR {
+        PLUS, MINUS, EQUALS;
+
+        int apply(int arg1, int arg2) throws CalcException {
+            switch (this) {
+
+                case PLUS:
+                    return arg1 + arg2;
+
+                case MINUS:
+                    return arg1 - arg2;
+            }
+
+            throw new CalcException("Unsupported operator: " + this);
+        }
+
+        static OPERATOR parse(String str) throws CalcException {
+            switch (str) {
+                case "+":
+                    return PLUS;
+                case "-":
+                    return MINUS;
+                case "=":
+                    return EQUALS;
+            }
+
+            throw new CalcException("Incorrect operator: " + str);
+        }
+    }
+}
