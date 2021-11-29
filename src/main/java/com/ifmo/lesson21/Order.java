a76ad885b3be98b6cef101f41cb684d5854ded2e
vikit <vikitutnet@mail.ru>
2021-11-29 13:09:31 +0300

    lesson 21_done

--- src/main/java/com/ifmo/lesson21/Order.java
+++ src/main/java/com/ifmo/lesson21/Order.java
@@ -4,11 +4,24 @@ public class Order {
     String nameDish;
 
     public Order (String str) {
-
         this.nameDish=str;
     }
 
     public String getNameDish() {
         return nameDish;
+    }
+
+    public synchronized void cooking(String str) {
+        int i = 0;
+        while (i > 0) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("Chef cooked "+str);
+        i++;
+        notify();
     }
 }
